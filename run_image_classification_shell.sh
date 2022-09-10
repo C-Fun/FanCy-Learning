e4ad91668e7f3e1a482398646023b9d6b928ee46
fangchen <cfun.orz@gmail.com>
2022-09-10 13:42:30 -0700

    shell

--- run_image_classification_shell.sh
+++ run_image_classification_shell.sh
@@ -11,4 +11,5 @@ python ./image_classification/train_val.py \
           --epochs 300 --learning_rate 1e-2 --lr_interval '0.6 0.8 0.9' --lr_reduce 5 --optimizer SGD \
           --log
 
-python ./image_classification/train_val.py --root ./ --seed 0 --devices 0 --dataset STL10 --im_size 128 --batch_size 8 --architecture mobilenetv2 --epochs 300 --learning_rate 1e-2 --lr_interval '0.6 0.8 0.9' --lr_reduce 5 --optimizer SGD --log
+# # command
+# python ./image_classification/train_val.py --root ./ --seed 0 --devices 0 --dataset STL10 --im_size 128 --batch_size 8 --architecture mobilenetv2 --epochs 300 --learning_rate 1e-2 --lr_interval '0.6 0.8 0.9' --lr_reduce 5 --optimizer SGD --log
